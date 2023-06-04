9d997845f89766cbb8699d40c6d705f0b3c0cb1d
PFKamberi <72415225+PFKamberi@users.noreply.github.com>
2023-06-05 00:01:11 +0300

    ratings.js

--- 2nd Assignment/ratings.js
+++ 2nd Assignment/ratings.js
@@ -1,23 +1,20 @@
-[
-  {
-    $group: {
-      _id: "$rating",
-      count: { $sum: 1 },
+db.getCollection('Netflix').aggregate(
+  [
+    {
+      $group: {
+        _id: '$rating',
+        count: { $sum: 1 }
+      }
     },
-  },
-  {
-    $sort: {
-      count: -1,
-    },
-  },
-  {
-    $limit: 20,
-  },
-  {
-    $project: {
-      _id: 0,
-      rating: "$_id",
-      count: 1,
-    },
-  },
-]
+    { $sort: { count: -1 } },
+    { $limit: 20 },
+    {
+      $project: {
+        _id: 0,
+        rating: '$_id',
+        count: 1
+      }
+    }
+  ],
+  { maxTimeMS: 60000, allowDiskUse: true }
+);
